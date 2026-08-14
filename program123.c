fb6d8b5a38252f8540469ce82d3ef475f282b379
Rupesh Kakde <146615091+Rupesh-Kakde@users.noreply.github.com>
2026-08-14 23:07:20 +0530

    Add files via upload

---
+++ program123.c
@@ -0,0 +1,79 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function name : Pattern
+//  Input :         Integer, Integer
+//  Output :        void
+//  Description :   Accept number of rows
+//                  and columns from user
+//                  and display lowercase
+//                  alphabets in odd rows
+//                  and numbers in even
+//                  rows.
+//  Author :        Rupesh Suresh Kakde
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0;
+    int j = 0;
+    char ch = '\0';
+
+    for(i = 1; i <= iRow; i++)
+    {
+        if((i % 2) != 0)
+        {
+            ch = 'a';
+
+            for(j = 1; j <= iCol; j++)
+            {
+                printf("%c\t",ch);
+                ch++;
+            }
+        }
+        else
+        {
+            for(j = 1; j <= iCol; j++)
+            {
+                printf("%d\t",j);
+            }
+        }
+
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of rows and columns : ");
+    scanf("%d %d",&iValue1,&iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
+
+//////////////////////////////////////////////////////////////////////////////////
+//
+//  Input  : iRow = 5   iCol = 5
+//  Output : a   b   c   d   e
+//           1   2   3   4   5
+//           a   b   c   d   e
+//           1   2   3   4   5
+//           a   b   c   d   e
+//
+//  Input  : iRow = 4   iCol = 3
+//  Output : a   b   c
+//           1   2   3
+//           a   b   c
+//           1   2   3
+//
+//////////////////////////////////////////////////////////////////////////////////
+
+// Time Complexity : O(iRow * iCol)
+// The outer loop executes iRow times and
+// the inner loop executes iCol times.
